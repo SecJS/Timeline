81833a1cfac77053b9d1588483bf5d4f5a35b049
João Lenon <lenonsec7@gmail.com>
2020-11-12 14:56:46 -0300

    feat: Initial Commit

--- build/dto/CreateTimelineDto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateTimelineDto = void 0;
-class CreateTimelineDto {
-}
-exports.CreateTimelineDto = CreateTimelineDto;
-//# sourceMappingURL=CreateTimelineDto.js.map
